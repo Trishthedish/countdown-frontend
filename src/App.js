d3a2fcd2e0bb591fffbd207a0fdf417a080ec6fd
trish <pmgriego@gmail.com>
2021-07-19 21:14:50 -0700

    clean up file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios'
+
 
 function App() {
+   const [countdowns, setCountdowns] = useState([])
+   console.log('countdowns: ', countdowns)
+   const [errorMessage, setErrorMessage] = useState('')
+
+   const getCountdowns = () => {
+     axios.get('http://localhost:5000/countdown')
+     .then((response) => {
+       console.log('response ---> ', response)
+       console.log('response.data?', response.data)
+       setCountdowns(response.data);
+       setErrorMessage('');
+     }).catch((error) => {
+       setErrorMessage('there has been an error: ', error);
+     });
+   };
+
+   const onButtonClick = () => {
+     getCountdowns();
+   };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Countdown Clock App</h1>
+      <button onClick={onButtonClick}>Get Count Down Events</button>
+      <div>
+        {countdowns.map(countdown =>
+          <ul>
+            <li>{countdown.title}</li>
+            <li>{countdown.countdown_till_date}</li>
+          </ul>
+        )}
+      </div>
+      <h1>{errorMessage}</h1>
     </div>
   );
 }
